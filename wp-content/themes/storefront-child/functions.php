13ae0c1af8251b7aa79138e691b7d928f9bc558c
Iskra152024 <inf@iskra.st>
2025-05-01 13:44:59 +0500

    feat: removing themes, creating CPT, taxonomy, metabox

---
+++ wp-content/themes/storefront-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+
+require_once get_stylesheet_directory() . '/inc/cities/custom-post-type.php';
+require_once get_stylesheet_directory() . '/inc/cities/custom-taxonomy.php';
+require_once get_stylesheet_directory() . '/inc/cities/metabox.php';
